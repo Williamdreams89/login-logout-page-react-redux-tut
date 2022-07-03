c73a2572c8c30984c75e83b0dedc484bf6a1308c
= <=>
2022-07-03 22:16:59 +0000

    Created the user reducer

---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Login() {
+  return (
+    <div><button>Log in</button></div>
+  )
+}
+
+export default Login
---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Profile() {
+  return (
+    <div>
+        <p>Name: </p>
+        <p>Age: </p>
+        <p>Email: </p>
+    </div>
+  )
+}
+
+export default Profile
